2dec4e5a2f57c42d25fae4f49542c7a66abbc69c
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-09-12 11:46:37 -0400

    Coding

---
+++ Jyan4/src/newIntro/IntroJava1.java
@@ -0,0 +1,14 @@
+package newIntro;
+
+public class IntroJava1 {
+
+	public static void main(String[] args) {
+		/*
+		 * declaration of a local variables 
+		 * notice the use of the word 'new'
+		 * new must be used to call a constructor 
+		 */
+		CodingConventions conventionsInstance = new CodingConventions();
+	}
+
+}
