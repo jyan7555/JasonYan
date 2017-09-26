81aefe30339d5e900d24fa697161fc1db71e4648
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-09-26 11:45:35 -0400

    Student

--- Jason Yan/src/attendance/Student.java
+++ Jason Yan/src/attendance/Student.java
@@ -1,7 +1,23 @@
 package attendance;
 
-public class Student implements Attendee(String firstname, String lastname){
-	public Student(){
-		
+public class Student implements Attendee{
+	private String firstName;
+	private String lastName;
+	private boolean present;
+	public Student(String firstName, String lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public boolean isPresent() {
+		return present;
+	}
+	public void setPresent() {
+		present = true;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastname() {
+		return lastName;
 	}
 }
