73a120eda9796858931d648c71389e86daa7a810
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-09-27 11:06:02 -0400

    !

--- Jason Yan/src/attendance/AttendanceMain.java
+++ Jason Yan/src/attendance/AttendanceMain.java
@@ -60,3 +60,4 @@ public class AttendanceMain {
 		System.out.print(printString);
 	}
 
+}
--- Jason Yan/src/attendance/Student.java
+++ Jason Yan/src/attendance/Student.java
@@ -7,17 +7,53 @@ public class Student implements Attendee{
 	public Student(String firstName, String lastName){
 		this.firstName = firstName;
 		this.lastName = lastName;
+		present = false;
 	}
 	public boolean isPresent() {
 		return present;
 	}
-	public void setPresent() {
-		present = true;
+	public void setPresent(boolean present) {
+		this.present = true;
 	}
 	public String getFirstName() {
 		return firstName;
 	}
-	public String getLastname() {
+	public String getLastName() {
 		return lastName;
+	}
+	public boolean mathces(String first, String last) {
+		return first.toLowerCase().equals(last.toLowerCase());
+	}
+	public boolean matches(String first) {
+		return firstName.toLowerCase().equals(first.toLowerCase());
+	}
+	public String getReportString() {
+		String output = "";
+		if (lastName.length() > 20) {
+			output += lastName.substring(0, 17) + "...";
+		}
+		else {
+			output += lastName;
+		}
+		while (output.length() < 20) {
+			output += " ";
+		}
+		if (firstName.length() > 20) {
+			output += firstName.substring(0, 17) + "...";
+		}
+		else {
+			output += firstName;
+		}
+		while (output.length() < 40) {
+			output += " ";
+		}
+		if (present) {
+			output += "Present";
+		}
+		else {
+			output += "Absent";
+		}
+		return output;
+		
 	}
 }
