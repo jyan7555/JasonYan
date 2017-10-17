0df29932b7393fabc8655688244a272fe915b8f9
BT_1N3_27 <BT_1N3_27@BT_1N3_27.bktech.local>
2017-10-17 11:45:37 -0400

    Array

--- Jason Yan/src/arrays/ArraysMain.java
+++ Jason Yan/src/arrays/ArraysMain.java
@@ -7,16 +7,102 @@ public class ArraysMain {
 	private int[] intRay;
 	
 	public ArraysMain() {
-		intRay = new int[100];
+		intRay = new int[10];
+		int[] randomRolls = new int[1000];
+		populate(randomRolls);
+		int[] result = longestConsecSeqAndPos(randomRolls);
+		int[] test3= {2,3,4,5,6,7,8,9,10,11,12};
+		//int[] test1 = {1,2,3,6,7,8,9,10,11,45,46,47};
+		int[] test2 = {10,11,12,13,14,15,6,7,8,9,10,11,45,46,47};
+		//int[] newRay = reverseOrder(intRay);
 		//populate(intRay);
 		//checkOccurences(intRay,2,12);
-		populate1ToN(intRay);
-		swap(intRay, 0, 1);
-		shuffle(intRay);
+		//populate1ToN(intRay);
+		//reverseOrder(intRay);
+		//swap(intRay, 0, 1);
+		//shuffle(intRay);
 		//Arrays is a Utility class included in Java
-		System.out.println(Arrays.toString(intRay));
+		System.out.println(longestConsecutiveSequence(test2));
+		System.out.println("The longest sequence of dice rolls is " + result[0]+
+				" it happened on the " + (result[1]+1) + "the roll. Starting with a roll of " + randomRolls[result[1]]+".");
 	}
-
+	public int[] longestConsecSeqAndPos(int[] arr) {
+		int count = 1;
+		int count2 = 1;
+		int pos = 0;
+		boolean newSeq = true;
+		int[] data = new int[2];
+		for (int i=0; i <= arr.length-2;i++) {
+			 if (arr[i] == arr[i+1]-1 ) {
+				count2++;
+				if (newSeq){
+					pos = i;
+					newSeq = false;
+				}
+			}
+			else {
+				if (count2 > count) {
+					count = count2;
+					count2 = 1;
+					newSeq= true;
+				}
+			}
+		}
+		data[0] = count;
+		data[1] = pos;
+		return data;
+	}
+	public int[] reverseOrder(int[] arr) {
+		int[] newArr = new int[arr.length];
+		for (int i=0; i <= arr.length-1;i++) {
+			newArr[i] = arr[arr.length-1-i];
+		}
+		return newArr;
+	}
+	public void reverseOrderOriginal(int[] arr) {
+		int[] newArr = new int[arr.length];
+		for (int i=0; i< arr.length; i++) {
+		    swap(arr, i, arr.length-1-i);
+		}
+	}
+	public int longestConsecutiveSequence(int[] arr) {
+		int count = 1;
+		int count2 = 1;
+	
+		for (int i=0; i <= arr.length-2;i++) {
+			 if (arr[i] == arr[i+1]-1 ) {
+				count2++;
+			}
+			else {
+				if (count2 > count) {
+					count = count2;
+					count2 = 1;
+				}
+			}
+		}
+		return count;
+	}
+	public int countLessThan(int[] arr, int n) {
+		int count = 0;
+		for (int value: arr) {
+			if (value < n) {
+					count++;
+			}
+		}
+		return count;
+	}
+	public void frontToBack(int[] arr) {
+		int num = arr[0];
+		for (int i=0; i <=arr.length-2; i++) {
+			arr[i] = arr[i+1];
+		}
+		arr[arr.length-1] = num;
+	}
+	public void cycleThrough(int[] arr, int n) {
+		for (int i=0; i <n; i++) {
+			frontToBack(arr);
+		}
+	}
 	private void shuffle(int[] arr) {
 		for (int i=0; i<arr.length-1; i++) {
 			swap(arr,(int)(Math.random()*arr.length),(int)(Math.random()*arr.length));
